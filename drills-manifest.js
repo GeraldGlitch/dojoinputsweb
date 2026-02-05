9ca71710534304b30ba6d53a157edec23d712bad
geraldb1996 <geraldb1996@gmail.com>
2026-02-04 20:38:36 -0600

    updating last drills

--- drills-manifest.js
+++ drills-manifest.js
@@ -1,15 +1,9 @@
 window.DRILLS_MANIFEST = {
-  "keyboard": [
-    "4Hit GreatSword combo - geraldb1996.dojofiles",
-    "DownLight to GroundPound - geraldb1996.dojofiles",
-    "Gravity Cancel DownLight to Recovery - geraldb1996.dojofiles",
-    "Throw Ax to NSig Imugi.dojofiles",
-    "testFile.dojofiles"
-  ],
+  "keyboard": [],
   "xbox360": [
-    "4Hit GreatSword combo - geraldb1996.dojofiles",
-    "DownLight to GroundPound - geraldb1996.dojofiles",
-    "Gravity Cancel DownLight to Recovery - geraldb1996.dojofiles",
+    "4Hit GreatSword combo.dojofiles",
+    "DownLight to GroundPound.dojofiles",
+    "Gravity Cancel DownLight to Recovery.dojofiles",
     "Throw Ax to NSig Imugi.dojofiles"
   ],
   "playstation": [],
